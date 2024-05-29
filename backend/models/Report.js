75a23a05bafb32b31df37ad55e4e0acc519c6e34
cindy.hii <134423669+chjy03@users.noreply.github.com>
2024-05-29 19:13:02 +0800

    Add date for Reports

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -1,10 +1,35 @@
 const mongoose = require('mongoose');
 
 const ReportSchema = new mongoose.Schema({
-    issueText: { // Change field name to issueText
+    issueText: {
+        type: String,
+        required: true
+    },
+    date: {
         type: String,
         required: true
     }
 });
 
 module.exports = mongoose.model('Report', ReportSchema);
+
+
+
+// const mongoose = require('mongoose');
+
+// const ReportSchema = new mongoose.Schema({
+//     issueText: { // Change field name to issueText
+//         type: String,
+//         required: true
+//     }
+// });
+
+// module.exports = mongoose.model('Report', ReportSchema);
+
+
+
+
+
+
+
+
